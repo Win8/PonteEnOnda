835ecd3347ac5c776265b93f4304c22ad28453b2
luisogh <luisogh@gmail.com>
2012-07-17 14:07:02 -0500

    mi noveno commit

---
+++ db/migrate/20120717183946_create_characters.rb
@@ -0,0 +1,13 @@
+class CreateCharacters < ActiveRecord::Migration
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.string :address
+      t.float :longitude
+      t.float :latitude
+      t.boolean :gmaps
+
+      t.timestamps
+    end
+  end
+end
