c102b861abb0b28f6b32c800ffe98ac3e1e8c644
luisogh <luisogh@gmail.com>
2012-07-17 14:04:56 -0500

    mi octavo commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PonteEnOnda::Application.config.secret_token = '48caacb135dc2ee389a228aab99577611439b8b946d380241ee0b4b08e58a0ca111ba4d08aeee77efcec9fab5c5c529a0ec2ee398ee9b160e71ac24d28948fa6'
